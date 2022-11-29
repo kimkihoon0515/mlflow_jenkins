c09f6d4d5af2a2aae8aaa67bd8e3c8fb0032b886
kimkihoon0515 <tega1996@naver.com>
2022-11-29 22:42:46 +0900

    bento build 기능 추가

--- service.py
+++ service.py
@@ -2,7 +2,7 @@ import bentoml
 import numpy as np
 from mlflow.tracking import MlflowClient
 import os
-import python
+
 os.environ["AWS_ACCESS_KEY_ID"] = "AKIAQHGXFPGFADDI35FY"
 os.environ["AWS_SECRET_ACCESS_KEY"] = "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"
 os.environ["MLFLOW_TRACKING_INSECURE_TLS"] = "True"
